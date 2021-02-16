0ea0f5a5e19387515aca68839326460b68211978
YuriiOnyskiv <w58996@student.wsiz.rzeszow.pl>
2021-02-16 13:10:14 +0100

    Adding tests

---
+++ Online_Examination_System/UnitTestOnlineExSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestOnlineExSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestOnlineExSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2291a372-d357-4651-8431-d0cc81b29abd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Online_Examination_System/UnitTestOnlineExSystem/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿
+
+
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestOnlineExSystem
+{
+
+
+    public class AccountService
+    {
+        public bool DoLogin(string userEmail, string password)
+        {
+            if (string.IsNullOrEmpty(userEmail) || string.IsNullOrEmpty(password))
+                return false;
+
+            return true;
+        }
+        public string EmptyData(string userEmail, string password)
+        {
+
+            userEmail = string.Empty;
+            password = string.Empty;
+            return "";
+        }
+        public bool DoRegister(string userName, string userLastName, int Age, string userEmail, string userPassword)
+        {
+            if (string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(userLastName) && string.IsNullOrEmpty(userEmail) &&
+                string.IsNullOrEmpty(userPassword))
+                return false;
+            else
+            {
+                return true;
+            }
+        }
+        public bool Age(int Age)
+        {
+            int _Age = Age;
+            if (_Age <= 5)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        [Test]
+        public void User_With_Correct_UserEmail_And_Password_Should_Login_Successfully()
+        {
+            // Arrange
+            var accountService = new AccountService();
+
+            // Act
+            bool result = accountService.DoLogin("test@mail.com", "test123");
+
+            // Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void User_With_Empty_UserEmail_And_Empty_Password()
+        {
+            // Arrange
+            var accountService = new AccountService();
+
+            // Act
+            var result = accountService.EmptyData(string.Empty, string.Empty);
+
+            // Assert
+            Assert.IsEmpty(result);
+
+        }
+
+        [Test]
+
+        public void Registration_User_With_Correct_Data()
+        {
+            // Arrange
+            var accountService = new AccountService();
+
+            // Act
+            bool result = accountService.DoRegister("Romek", "Sparrow", 28, "Romek.1989@mail.com", "pass123");
+
+            // Assert
+            Assert.IsTrue(result);
+
+        }
+        [Test]
+
+        public void Age_Validation()
+        {
+            // Arrange
+            var accountService = new AccountService();
+
+            // Act
+            bool result = accountService.Age(6);
+
+            // Assert
+            Assert.IsTrue(result);
+
+        }
+
+
+    }
+}
